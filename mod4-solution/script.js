549be6d9fb2cd3561ee0e22efd12b4306b72df31
=Pavnesh <=2019kuec2037@iiitkota.ac.in>
2020-06-30 16:43:48 +0530

    Update script.js

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -13,6 +13,8 @@ Hello Laura
 Good Bye Jim
 */
 
+
+
 (function() {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
